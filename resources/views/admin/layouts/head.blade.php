380a036cc9dfdd4a17dbab12917295de78525594
lamzpq19961228 <2597625216@qq.com>
2020-04-11 16:34:29 +0800

    11

--- resources/views/admin/layouts/head.blade.php
+++ resources/views/admin/layouts/head.blade.php
@@ -12,5 +12,8 @@
         @section('sidebar')
             这里是侧边栏
         @show
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,10 +1,13 @@
 @extends('admin.layouts.head')
 
 @section('header')
-    @parent
-    <p>Laravel学院致力于提供优质Laravel中文学习资源</p>
+    
 @endsection
 
 @section('sidebar')
-    <p>这里是主体内容，完善中...</p>
+    
+@endsection
+
+@section('content')
+    
 @endsection

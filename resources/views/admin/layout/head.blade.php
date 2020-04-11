cbd7b929ad926f47b93a1bd294172a4d995bde1a
lamzpq19961228 <2597625216@qq.com>
2020-04-11 16:22:44 +0800

    11

---
+++ resources/views/admin/layout/head.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title','Laravel')</title>
+    </head>
+    <body>
+        @section('header')
+            这里是头部栏
+        @show
+        @section('sidebar')
+            这里是侧边栏
+        @show
+    </body>
+</html>

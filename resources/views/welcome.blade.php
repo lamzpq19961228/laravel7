fa1ab8a2ce3595bc620ec97fe15eb0ba44cfc57b
lamzpq19961228 <2597625216@qq.com>
2020-04-11 11:17:32 +0800

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    hello7
+                    hello8
                 </div>
 
                 <div class="links">

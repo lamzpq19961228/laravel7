e87552c78a198c489e96c86998844608f277bb1e
lamzpq19961228 <2597625216@qq.com>
2020-04-13 16:05:21 +0800

    1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 class AdminController extends Controller
 {
-    public function login(){
-    	return view('admin.login');
+    public function index()
+    {
+        //获取登录用户
+        $user = Auth::user();
+
+        //判断是否登录
+        if (!$user) {
+            login();
+        } else {
+            return view('admin.index');
+        }
+    }
+    public function login()
+    {
+        return view('admin.login');
     }
 }
